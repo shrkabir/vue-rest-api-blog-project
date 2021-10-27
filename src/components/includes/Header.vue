e51280e55b0d09524b7fceb8a38e01863cef9aa1
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2021-10-27 13:34:40 +0600

    All published categories has shown in the Header component through API.

--- src/components/includes/Header.vue
+++ src/components/includes/Header.vue
@@ -3,27 +3,10 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container px-5">
                     <a class="navbar-brand" href="index.html">My Blog</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-                            <li class="nav-item"><a class="nav-link" href="pricing.html">Pricing</a></li>
-                            <li class="nav-item"><a class="nav-link" href="faq.html">FAQ</a></li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
-                                    <li><a class="dropdown-item" href="blog-home.html">Blog Home</a></li>
-                                    <li><a class="dropdown-item" href="blog-post.html">Blog Post</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdownPortfolio" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Portfolio</a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownPortfolio">
-                                    <li><a class="dropdown-item" href="portfolio-overview.html">Portfolio Overview</a></li>
-                                    <li><a class="dropdown-item" href="portfolio-item.html">Portfolio Item</a></li>
-                                </ul>
+                            <li class="nav-item" v-for="category in categories" :key="category.id">
+                                <a class="nav-link" href="index.html">{{category.category_name}}</a>
                             </li>
                         </ul>
                     </div>
@@ -38,7 +21,7 @@ export default {
 
   data () {
     return {
-
+      categories: []
     }
   },
 
@@ -47,6 +30,16 @@ export default {
   },
 
   methods: {
+    allPublishedCategories: function () {
+      this.$http.get('http://localhost/laravel-blog-project/public/api/categories/published-categories')
+        .then(function (response) {
+          this.categories = response.body
+        })
+    }
+  },
+
+  created () {
+    this.allPublishedCategories()
   }
 }
 </script>
